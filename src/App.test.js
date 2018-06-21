e5e868eac761d3bcb448eed60b1fc1b452ff0ce5
mnap <learn@10g.pl>
2018-06-22 01:06:43 +0200

    Add App component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
+/* eslint-env jest */
 import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
 
 it('renders without crashing', () => {
     shallow(<App />);
+});
+
+it('should update player score', () => {
+    const appComponent = shallow(<App />);
+    const players = [
+        {
+            name: 'Kunegunde',
+            score: 5
+        }
+    ];
+    appComponent.setState({players});
+
+    const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+    onScoreUpdate(0, 5);
+
+    const playersAfterUpdate = appComponent.state().players;
+
+    expect(playersAfterUpdate[0].score).toEqual(10);
 });
