71be4f038a5157086f92e1bceeb7f9e4300f6155
mnap <learn@10g.pl>
2018-07-18 23:02:29 +0200

    Update PlayersList component test with onPlayerRemove props

--- src/components/PlayersList/PlayersList.test.js
+++ src/components/PlayersList/PlayersList.test.js
@@ -53,3 +53,31 @@ it('should call onScoreUpdate', () => {
 
     expect(mockedOnScoreUpdate).toBeCalledWith(1, 8);
 });
+
+it('should call onPlayerRemove', () => {
+    const players = [
+        {
+            name: 'Kunegunde',
+            score: 5
+        },
+        {
+            name: 'Hermenegilde',
+            score: 0
+        }
+    ];
+    const mockedOnPlayerRemove = jest.fn();
+
+    const playerComponent = shallow(
+        <PlayersList
+            players={players}
+            onPlayerRemove={mockedOnPlayerRemove}
+        />
+    );
+
+    const lastPlayer = playerComponent.find(Player).last();
+    const onPlayerRemove = lastPlayer.prop('onPlayerRemove');
+
+    onPlayerRemove();
+
+    expect(mockedOnPlayerRemove).toBeCalledWith(1);
+});
