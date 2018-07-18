54f934366f0599bc3b5b68fda09894a917f3d3d5
mnap00 <learn@10g.pl>
2018-07-18 11:02:51 +0200

    Fix onSubmit variable scope

--- src/components/AddPlayer/AddPlayer.js
+++ src/components/AddPlayer/AddPlayer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './AddPlayer.css';
 
-onSubmit = (event) => {
-    event.preventDefault();
-    props.onPlayerAdd(input.value);
-    input.value = '';
-};
 
 const AddPlayer = (props) => {
     let input;
+    const onSubmit = (event) => {
+        event.preventDefault();
+        props.onPlayerAdd(input.value);
+        input.value = '';
+    };
+
     return (
         <form className='AddPlayer' onSubmit={onSubmit}>
             <input
