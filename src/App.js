35984bc486d6ed3932d9324bf2e3b0f33be95e9f
mnap00 <learn@10g.pl>
2018-07-18 11:05:57 +0200

    Update App component with AddPlayer functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 import PlayersList from './components/PlayersList/PlayersList';
 
 class App extends Component {
@@ -9,6 +10,18 @@ class App extends Component {
         this.state = {
             players: []
         };
+    }
+
+    onPlayerAdd = (playerName) => {
+        const newPlayer = {
+            name: playerName,
+            score: 0,
+        };
+        if (playerName) {
+            this.setState({
+                players: [...this.state.players, newPlayer]
+            });
+        }
     }
 
     onScoreUpdate = (playerIndex, scoreChange) => {
@@ -25,6 +38,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <AddPlayer onPlayerAdd={this.onPlayerAdd} />
                 <PlayersList
                     players={this.state.players}
                     onScoreUpdate={this.onScoreUpdate}
